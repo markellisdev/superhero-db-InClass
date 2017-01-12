f22bb9ba2070a9396317aefd32cf458b73929741
Mark Ellis <markellisdev@gmail.com>
2017-01-12 14:52:13 -0600

    added superhero and sidekick table

---
+++ superhero.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `Superhero` (
+	`SuperheroId`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`Name`  TEXT NOT NULL,
+	`Gender` TEXT NOT NULL,
+	`Lair` TEXT NOT NULL
+);
+
+CREATE TABLE `Sidekick` (
+	`SidekickId`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`Name`  TEXT NOT NULL,
+	`Gender` TEXT NOT NULL,
+	`Profession` INTEGER NOT NULL,
+	`SuperheroId` TEXT NOT NULL,
+	FOREIGN KEY(`SuperheroId`) REFERENCES `Superhero`(`SuperheroId`)
+);
